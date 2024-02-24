3e21baa5ae132cb49cff85d55459a80cb0d307dc
Your Namearmanshehad <shahed15-13552@diu.edu.bd>
2024-02-24 22:05:23 +0600

    first commit

---
+++ components/footer.php
@@ -0,0 +1,35 @@
+<!-- footer section starts  -->
+
+<footer class="footer">
+
+   <section class="flex">
+
+      <div class="box">
+         <a href="tel:1234567890"><i class="fas fa-phone"></i><span>01989435570</span></a>
+         <a href="tel:1112223333"><i class="fas fa-phone"></i><span>101989435570</span></a>
+         <a href="mailto:shaikhanas@gmail.com"><i class="fas fa-envelope"></i><span>shahed15@diu.edu.bd</span></a>
+         <a href="#"><i class="fas fa-map-marker-alt"></i><span>Dhaka, Bangladesh</span></a>
+      </div>
+
+      <div class="box">
+         <a href="home.php"><span>home</span></a>
+         <a href="about.php"><span>about</span></a>
+         <a href="contact.php"><span>contact</span></a>
+         <a href="saved.php"><span>saved properties</span></a>
+      </div>
+
+      <div class="box">
+         <a href="https://www.facebook.com/shehad.arman134/" target="_blank"><span>facebook</span><i class="fab fa-facebook-f"></i></a>
+         <a href="#"><span>twitter</span><i class="fab fa-twitter"></i></a>
+         <a href="https://www.linkedin.com/in/md-shahed-arman-shehad-9ab2a5202/" target="_blank"><span>linkedin</span><i class="fab fa-linkedin"></i></a>
+         <a href="#"><span>instagram</span><i class="fab fa-instagram"></i></a>
+
+      </div>
+
+   </section>
+
+   <div class="credit">&copy; copyright @ <?= date('Y'); ?> by <span>Md. Shahed Arman Shehad</span> | all rights reserved!</div>
+
+</footer>
+
+<!-- footer section ends -->
